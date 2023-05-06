1f701120c8fa6d832b2dd034e34a112fa7e03b28
srobledo2021 <s.robledo.2021@alumnos.urjc.es>
2023-05-06 19:40:41 +0200

    porisaca

---
+++ navigate_bt/include/navigate_bt/RequestName.hpp
@@ -0,0 +1,54 @@
+#ifndef NAVIGATE_BT__REQUESTNAME_HPP_
+#define NAVIGATE_BT__REQUESTNAME_HPP_
+
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sound_play.hpp"
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+namespace navigate_bt
+{
+
+class RequestName : public BT::ActionNodeBase
+{
+public:
+  explicit RequestName(
+    const std::string & xml_tag_name,
+    const BT::NodeConfiguration & conf);
+
+
+  void RequestNameCB(dialogflow_ros2_interfaces::msg::DialogflowResult result)
+  {
+    RCLCPP_INFO(this->get_logger(), "[RequestName] RequestNameCB: intent [%s]", result.intent.c_str());
+    speak(result.fulfillment_text);
+  }
+
+   void noIntentCB(dialogflow_ros2_interfaces::msg::DialogflowResult result)
+  {
+    RCLCPP_INFO(this->get_logger(), "[RequestName] noIntentCB: intent [%s]", result.intent.c_str());
+  }
+
+  void halt();
+  BT::NodeStatus tick();
+
+  static BT::PortsList providedPorts()
+  {
+    return{
+      BT::OutputPort<std::string>("nameID");
+      };
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  gb_dialog::DialogInterface dialog_;
+  rclcpp::Time name ="";
+  bool listening_ = false;
+};
+
+}  // namespace navigate_bt
+
+#endif  // NAVIGATE_BT__REQUESTNAME_HPP_
---
+++ navigate_bt/src/navigate_bt/RequestName.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <iostream>
+
+#include "navigate_bt/RequestName.hpp"
+#include "gb_dialog/DialogInterface.hpp"
+#include "behaviortree_cpp_v3/behavior_tree.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::placeholders;
+
+using namespace std::chrono_literals;
+
+namespace navigate_bt
+{
+
+RequestName:RequestName(
+  const std::string & xml_tag_name,
+  const BT::NodeConfiguration & conf)
+: BT::ActionNodeBase(xml_tag_name, conf)
+{
+  dialog_.registerCallback(
+    std::bind(&RequestName::noIntentCB, this, _1))
+  dialog_.registerCallback(
+    std::bind(&RequestName::RequestNameCB, this, _1),"request_name")
+  config().blackboard->get("node",node_);
+}
+
+RequestName::halt(){
+}
+
+RequestName::tick()
+(
+  if (status() == BT::NodeStatus::IDLE) {
+    start_time_ = node_->now();
+    dialog_.speak("Howdy! What is your name?");
+  }
+  rclcpp::spin_some(dialog_.get_node_base_interface());
+
+  if(name_ == "" && node_->now() - start_time_ > 10s){
+    RCLCPP_INFO(node_->get_logger(),"Reboot the system");
+    listening_ = false;
+    return BT::NodeStatus::FAILURE;
+  }
+
+  if(node_->now() - start_time_  < 3s){
+    return BT::NodeStatus::RUNNING;
+  }
+
+  if(!listening_){
+    listening_ = true;
+    dialog_.listen();
+  }
+
+  if(name_== ""){
+    return BT::NodeStatus::RUNNING;
+  }
+
+  dialog_.speak("Hello "+ name_ + "Nice to meet you. You are really welcomed!");
+
+  setOutput("nameID",name_);
+
+  name_="";
+  listening_ = false;
+
+  return BT::NodeStatus::SUCCESS;
+
+)
+
+}  // namespace navigate_bt
